81d096c979be04dcada55580be0b849b6dd53bba
Rajpateriya <pateriyaraj52@gmail.com>
2024-06-06 00:37:07 +0530

    base url updated

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:5000/api/v1/";
+const BASE_URL = "https://expense-tracker-backend-hn5w.onrender.com/api/v1/";
 
 
 const GlobalContext = React.createContext()
