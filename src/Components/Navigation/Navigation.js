044aba1a8f76b56ac73f53392664b4650772465a
Rajpateriya <pateriyaraj52@gmail.com>
2024-06-15 14:20:25 +0530

    navigation updated

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -11,7 +11,7 @@ function Navigation({active, setActive}) {
             <div className="user-con">
                 <img src={avatar} alt="" />
                 <div className="text">
-                    <h2>Mike</h2>
+                    <h2>Raj</h2>
                     <p>Your Money</p>
                 </div>
             </div>
